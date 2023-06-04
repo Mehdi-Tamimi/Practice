f6015c6b68609d71a5d4056a4cd8bd916072d638
Mehdi Tamimi <contactwithtamimimehdi@gmail.com>
2023-06-04 17:53:37 +0330

    twoSum finished

---
+++ Two Sum/twoSum.js
@@ -0,0 +1,31 @@
+
+
+function twoSum(numbers,target) {
+    const answers = []
+    const checked = []
+    const list = []
+    numbers = numbers.filter((value) => {
+        return value < target
+    })
+    numbers = numbers.filter((value) => {
+
+        if (!(list.includes(value))) {
+            list.push(value)
+            return value
+        } 
+    })
+    console.log(numbers)
+    for (const i of numbers) {
+        if (!checked.includes(i)) {
+            checked.push(i)
+            if (numbers.includes(target-i)) {
+                checked.push(target-i)
+                answers.push([numbers.indexOf(i),numbers.indexOf(target-i)])
+            }
+        }
+        
+    }
+    return answers
+    
+}
+console.log(twoSum([1,3,5,6,3,6,8,9,7,7],9))
